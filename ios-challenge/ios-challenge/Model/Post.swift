f6394265c1769c5c3a57621a4bc96937aba51cdb
Matias Glessi <matiasglessi@gmail.com>
2020-10-05 19:55:48 -0300

    Add Post model

---
+++ ios-challenge/ios-challenge/Model/Post.swift
@@ -0,0 +1,22 @@
+//
+//  Post.swift
+//  ios-challenge
+//
+//  Created by Matias Glessi on 05/10/2020.
+//
+
+import Foundation
+
+enum ReadStatus {
+    case read
+    case unread
+}
+
+struct Post {
+    let title: String
+    let author: String
+    let date: Date
+    let thumbnail: String
+    let comments: Int
+    let status: ReadStatus
+}
