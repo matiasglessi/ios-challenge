9dd65cbd2162f03d92b6e971839ec5d0462c734a
Matias Glessi <matiasglessi@gmail.com>
2020-10-08 11:47:43 -0300

    Add DetailView UI

---
+++ ios-challenge/ios-challenge/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  ios-challenge
+//
+//  Created by Matias Glessi on 05/10/2020.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var postDescriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
